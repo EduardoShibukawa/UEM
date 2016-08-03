1f055000c201af4c6c15f14d830992922a26e600
Eduardo Henrique Kenji Shibukawa <EduardoShibuka@gmail.com>
2016-08-03 00:59:41 -0300

    Terminado 2º problema
    Terminado 2 problema

--- PPLF/Trabalho-2/p2.pl
+++ PPLF/Trabalho-2/p2.pl
@@ -1,26 +1,29 @@
-solucao_todas(_, [], _) :- !.
-solucao_todas(Q, [V|VS], N) :-
-	N0 is N + 1,
+solucao_peca(_, [], _) :- !.
+solucao_peca(Q, [V|VS], N) :-
 	QV0 is Q + N,
 	QV1 is Q - N,
 	V \= QV0,
 	V \= QV1,
 	V \= Q,
-	solucao_todas(Q, VS, N0).
+	N0 is N + 1,
+	solucao_peca(Q, VS, N0).
+
+solucao([]).
+
+solucao([Q|QS]) :-
+	solucao_peca(Q, QS, 1),
+	solucao(QS).
 
 solucao(Q, VS) :-
-	solucao_todas(Q, VS, 1).
-
-solucao([Q,V|VS]) :-
-	solucao_todas(Q, [V|VS], 1).
+	solucao_peca(Q, VS, 1).
 
 rainhas([], _).
 rainhas([Q|QS], N) :-
 	rainhas(QS, N),
 	findall(X, between(1, N, X), L),
 	member(Q0, L),
-	Q = Q0,
-	solucao(Q, QS).
+	solucao(Q0, QS),
+	Q = Q0.
 
 rainhas_p(QS, N) :-
 	nonvar(N),
