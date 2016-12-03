fe0fd57881cde21c97091508b2a829edfd5dcfe3
Eduardo Henrique Kenji Shibukawa <eduardoshibuka@gmail.com>
2016-12-03 17:52:41 -0200

    Terminado algoritmo parelelo
    Terminado algoritmo paralelo do problema do mercado

--- PC/memoria_compartilhada/memoria_compartilhada.c
+++ PC/memoria_compartilhada/memoria_compartilhada.c
@@ -2,17 +2,45 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdbool.h>
-#include <zconf.h>
+#include <stdlib.h>
 
-#define NUM_FILAS 3
-#define NUM_THREADS_CLIENTE 15
-#define NUM_CLIENTES_POR_THREAD 10
+#define NUM_FILAS 5
+#define NUM_THREADS_CLIENTE 5
+#define NUM_CLIENTES_POR_THREAD 10000000
 
 sem_t sem_fila[NUM_FILAS];
 sem_t sem_cliente[NUM_FILAS];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t barrier;
 
 int tamanho_fila[NUM_FILAS];
+int clientes[NUM_THREADS_CLIENTE * NUM_CLIENTES_POR_THREAD];
+int index_cliente;
+int lojaAberta;
+
+void inicializar() {
+    index_cliente = -1;
+    lojaAberta = true;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_barrier_init(&barrier, NULL, NUM_THREADS_CLIENTE);
+
+    for (int i = 0; i < NUM_FILAS; i++)
+        sem_init(&sem_fila[i], 0, 0);
+
+    for (int i = 0; i < NUM_THREADS_CLIENTE; i++)
+        sem_init(&sem_cliente[i], 0, 0);
+}
+
+void finalizar() {
+    pthread_mutex_destroy(&mutex);
+
+    for (int i = 0; i < NUM_FILAS; i++)
+        sem_destroy(&sem_fila[i]);
+
+    for (int i = 0; i < NUM_THREADS_CLIENTE; i++)
+        sem_destroy(&sem_cliente[i]);
+}
 
 int buscarFilaComMenorTamanho() {
     if (tamanho_fila[0] == 0)
@@ -32,61 +60,66 @@ int buscarFilaComMenorTamanho() {
     return menorFila;
 }
 
-void serAtendido(int fila) {
-    printf("Cliente sendo atendido caixa %d \n", fila);
-    sleep(1);
+void serAtendido(int cliente) {
+    printf("Cliente %d sendo atendido\n", cliente);
+}
+
+int getIdCliente() {
+    return rand() % 10000;
 }
 
 void novoCliente() {
-    pthread_mutex_lock(&mutex);
+    int cliente = getIdCliente();
+    printf("Cliente %d chegou.\n", cliente);
 
+    pthread_mutex_lock(&mutex);
+    index_cliente += 1;
+    clientes[index_cliente] = cliente;
     int menorFila = buscarFilaComMenorTamanho();
     sem_post(&sem_cliente[menorFila]);
     tamanho_fila[menorFila] += 1;
     pthread_mutex_unlock(&mutex);
 
-    printf(
-            "Cliente entrando na fila %d, de tamanho %d \n",
-            menorFila, tamanho_fila[menorFila]
-    );
+    printf("Cliente %d entrou na fila %d.\n", cliente, menorFila);
 
     sem_wait(&sem_fila[menorFila]);
-    serAtendido(menorFila);
+    serAtendido(cliente);
 }
 
-int todasFilasVazias(){
+int todasFilasVazias() {
     pthread_mutex_lock(&mutex);
 
     int todasVazias = true;
     for (int i = 0; i < NUM_FILAS; i++)
-        if (tamanho_fila[i] > 0){
+        if (tamanho_fila[i] > 0) {
             todasVazias = false;
             break;
         }
 
     pthread_mutex_unlock(&mutex);
-
     return todasVazias;
 }
 
-void atenderCliente(int fila){
-    printf("Caixa %d atendendo cliente \n", fila);
-    sleep(1);
+void atenderCliente(int fila, int cliente) {
+    printf("Caixa %d atendendo cliente %d \n", fila, cliente);
 }
 
-int atenderClienteFila(int fila){
+int atenderClienteFila(int fila_origem, int fila_atendendo) {
     pthread_mutex_lock(&mutex);
-    if (tamanho_fila[fila] > 0){
-        tamanho_fila[fila] -= 1;
-        printf(
-                "Cliente indo para atendimento na fila %d, de tamanho %d \n",
-                fila, tamanho_fila[fila]
-        );
-        sem_post(&sem_fila[fila]);
+    if (tamanho_fila[fila_origem] > 0) {
+        int cliente = clientes[index_cliente];
+        index_cliente -= 1;
+        tamanho_fila[fila_origem] -= 1;
+        sem_post(&sem_fila[fila_origem]);
+
         pthread_mutex_unlock(&mutex);
 
-        sem_wait(&sem_cliente[fila]);
-        atenderCliente(fila);
+        sem_wait(&sem_cliente[fila_origem]);
+        if (fila_origem != fila_atendendo) {
+            printf("Cliente %d saiu do caixa %d para o caixa %d.\n",
+                   cliente, fila_origem, fila_atendendo);
+        }
+        atenderCliente(fila_atendendo, cliente);
 
         return true;
     }
@@ -95,70 +128,89 @@ int atenderClienteFila(int fila){
     return false;
 }
 
-void atenderTodosClienteFila(int fila){
-    while (atenderClienteFila(fila));
+void atenderTodosClienteFila(int fila) {
+    while (atenderClienteFila(fila, fila));
 }
 
-void filaVaziaAtenderOutraFila(int fila_origem){
-    for (int i = 0; i < NUM_FILAS; i++){
-        if ((fila_origem != i)
-            && (atenderClienteFila(i)))
+void filaVaziaAtenderOutraFila(int fila) {
+    for (int i = 0; i < NUM_FILAS; i++) {
+        if ((fila != i)
+            && (atenderClienteFila(i, fila)))
             break;
     }
 }
 
-void bloquearFilaSeTodasEstiveremVazia(int fila){
-    if (todasFilasVazias()) {
+void bloquearFilaSeTodasEstiveremVazia(int fila) {
+    if (todasFilasVazias()
+        && lojaAberta) {
         printf("bloqueando fila %d vazia. \n", fila);
         sem_wait(&sem_cliente[fila]);
     }
 }
 
+void desbloquearTodosFilas() {
+    for (int i = 0; i < NUM_FILAS; i++) {
+        sem_post(&sem_cliente[i]);
+    }
+}
+
+int continuarAtendendo() {
+    return lojaAberta || !todasFilasVazias();
+}
+
 void atendimentoFila(int fila) {
-    while (true){
+    while (continuarAtendendo()) {
         atenderTodosClienteFila(fila);
         filaVaziaAtenderOutraFila(fila);
         bloquearFilaSeTodasEstiveremVazia(fila);
     }
 }
 
-
-void inicializarSemaforos() {
-    pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < NUM_FILAS; i++) {
-        sem_init(&sem_fila[i], 0, 0);
-        sem_init(&sem_cliente[i], 0, 0);
-    }
-}
-
 void *threadNovosCliente(void *args) {
     for (int i = 0; i < NUM_CLIENTES_POR_THREAD; i++)
         novoCliente();
+
+    if (pthread_barrier_wait(&barrier) != 0) {
+        lojaAberta = false;
+        if (todasFilasVazias())
+            desbloquearTodosFilas();
+    }
+
+    pthread_exit(NULL);
 }
 
 void *threadAtenderFila(void *fila) {
     int index = (int) fila;
+
     atendimentoFila(index);
+
     pthread_exit(NULL);
 }
 
 int main() {
+    int i;
+    srand((unsigned int) time(NULL));
     pthread_t tfila[NUM_FILAS];
     pthread_t tclientes[NUM_THREADS_CLIENTE];
 
-    inicializarSemaforos;
+    inicializar();
 
-    for (int i = 0; i < NUM_THREADS_CLIENTE; i++)
-        if (pthread_create(&tclientes[i], NULL, &threadNovosCliente, NULL)){
+    for (i = 0; i < NUM_THREADS_CLIENTE; i++)
+        if (pthread_create(&tclientes[i], NULL, &threadNovosCliente, NULL)) {
             printf("Erro na criacao da thread.");
         };
 
-    sleep(3);
-
-    for (int i = 0; i < NUM_FILAS; i++)
-        if (pthread_create(&tfila[i], NULL, &threadAtenderFila, (void *)i)){
+    for (i = 0; i < NUM_FILAS; i++)
+        if (pthread_create(&tfila[i], NULL, &threadAtenderFila, (void *) i)) {
             printf("Erro na criacao da thread.");
         };
 
-    pthread_exit(NULL);
+    for (i = 0; i < NUM_FILAS; i++) {
+        pthread_join(tfila[i], NULL);
+    }
+
+    finalizar();
 }
+
+
+
