58d196cdbe6c7c85a883d34736c64adcb2e53246
oassam <ra65135@uem.br>
2016-12-07 08:21:58 -0200

    Removido linhas de código comentadas do algoritmo sequencial.

--- PC/memoria_compartilhada/sequencial.c
+++ PC/memoria_compartilhada/sequencial.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <semaphore.h>
 #include <stdbool.h>
-#include <pthread.h>
 #include <time.h>
 
-#define NUM_FILAS 15
-#define NUM_CLIENTES 10
-
-//sem_t semClientes;
-//sem_t semFilas[NUM_FILAS];
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#define NUM_FILAS 21
+#define NUM_CLIENTES 10000
 
 int clientes[NUM_CLIENTES];
 int tamanhoFila[NUM_FILAS];
@@ -21,15 +14,15 @@ int quantidadeZeros;
 /// <summary>
 /// Encontra a quantidade de algarismos o valor possui.
 /// </summary>
-/// <param name="valor">Valor do inteiro a ser calculado.</param>
+/// <param name="valor" type="int">Valor do inteiro a ser calculado.</param>
 /// <returns>Retorna o tamanho, quantidade, de algarismos o valor informado possui.</returns>
 int tamanhoDigito(int valor)
 {
     int tamanho = 1;
-    if(valor == 0)
+    if (valor == 0)
         return tamanho;
 
-    for(tamanho = 0; valor > 0; valor /= 10)
+    for (tamanho = 0; valor > 0; valor /= 10)
         tamanho++;
 
     return tamanho;
@@ -41,14 +34,14 @@ int tamanhoDigito(int valor)
 /// <returns>Retorno, um inteiro, o identificador criado para o cliente.</returns>
 int gerarIdCliente()
 {
-    return (rand() % (NUM_CLIENTES * 10)) + 1;
+    return (rand() % NUM_CLIENTES) + 1;
 }
 
 /// <summary>
 /// Método que informa em qual caixa o cliente será atendido.
 /// </summary>
-/// <param name="idClienteAtendido">Identificador do cliente a ser atendido.</param>
-/// <param name="idFilaAtendido">Identificador da fila no qual o cliente foi atendido.</param>
+/// <param name="idClienteAtendido" type="int">Identificador do cliente a ser atendido.</param>
+/// <param name="idFilaAtendido" type="int">Identificador da fila no qual o cliente foi atendido.</param>
 void atenderCliente(int idClienteAtendido, int idFilaAtendido)
 {
     printf("Cliente %0*d atendido no caixa %0*d.\n", quantidadeZeros, idClienteAtendido, quantidadeZeros, idFilaAtendido);
@@ -92,17 +85,13 @@ int recuperarFilaMaior()
 /// <summary>
 /// Adiciona o cliente na menor fila.
 /// </summary>
-/// <param name="idCliente">Identificador do cliente a ser inserido.</param>
+/// <param name="idCliente" type="int">Identificador do cliente a ser inserido.</param>
 /// <returns>Retorna, um inteiro, o identificador da fila que o cliente foi inserido.</returns>
 int inserirCliente(int idCliente)
 {
-    pthread_mutex_lock(&mutex);
     clientes[++indiceCliente] = idCliente;
     int idFilaMenor = recuperarFilaMenor();
-    //sem_post(&semClientes);
     tamanhoFila[idFilaMenor]++;
-    pthread_mutex_unlock(&mutex);
-    //sem_wait(&semFilas[idFilaMenor]);
 
     return idFilaMenor;
 }
@@ -110,21 +99,15 @@ int inserirCliente(int idCliente)
 /// <summary>
 /// Ŕealiza o atendimento da fila desejada.
 /// </summary>
-/// <param name="idFilaAtendida">Identificador da fila atendida.</param>
+/// <param name="idFilaAtendida" type="int">Identificador da fila atendida.</param>
 /// <returns>Retorna, um inteiro, se a fila foi atendida [1] ou não [0].</returns>
 int realizarAtendimentoFila(int idFilaAtendida)
 {
     if (tamanhoFila[idFilaAtendida] < 1)
         return false;
 
-    pthread_mutex_lock(&mutex);
-
     int idClienteAtendido = clientes[indiceCliente--];
     tamanhoFila[idFilaAtendida]--;
-    //sem_post(&semFilas[idFilaAtendida]);
-    //sem_wait(&semClientes);
-
-    pthread_mutex_unlock(&mutex);
 
     atenderCliente(idClienteAtendido, idFilaAtendida);
 
@@ -134,7 +117,7 @@ int realizarAtendimentoFila(int idFilaAtendida)
 /// <summary>
 /// Realiza o atendimento de todos os clientes da fila
 /// </summary>
-/// <param name="idFilaAtendida">Identificador da fila atendida.</param>
+/// <param name="idFilaAtendida" type="int">Identificador da fila atendida.</param>
 void atenderTodosClienteFila(int idFilaAtendida)
 {
     while (realizarAtendimentoFila(idFilaAtendida));
@@ -151,28 +134,16 @@ void inicializar()
     printf("Total de clientes: \t%d\n", NUM_CLIENTES);
     printf("Total de caixas: \t%d\n\n", NUM_FILAS);
 
-    pthread_mutex_init(&mutex, NULL);
-
     int contador;
-    for (contador = 0;  contador < NUM_FILAS; contador++){
-        //sem_init(&semFilas[contador], 0, 0);
+    for (contador = 0;  contador < NUM_FILAS; contador++)
         tamanhoFila[contador] = 0;
-    }
-
-    //sem_init(&semClientes, 0, 0);
 }
 
 /// <summary>
 /// Método que finaliza as principais variáveis do programa.
 /// </summary>
 void finalizar() {
-    pthread_mutex_destroy(&mutex);
     printf("Finalizado com sucesso!\n\n");
-    //int contador;
-    //for (contador = 0; contador < NUM_FILAS; contador++)
-        //sem_destroy(&semFilas[contador]);
-
-    //sem_destroy(&semClientes);
 }
 
 /// <summary>
